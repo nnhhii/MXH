0aad3844a37f5074e876383f9f38f29400b9d223
AnhKiet1234 <2254810125@vaa.edu.vn>
2024-03-02 14:09:16 +0700

    them

--- menu/daluu.php
+++ menu/daluu.php
@@ -1,8 +1,65 @@
-
+<style> 
+.layout{
+    height: 220px;
+    width: 1200px;
+    margin: 20px 0 0 20px;
+    border-radius: 10px;
+    background-color:blue
+}
+.anh{
+    height: 190px;
+    width: 190px;
+    float:left;
+    border-radius: 10px;
+    margin: 15px 0 0 20px;
+  
+    background-color:red
+}
+.nut_them{
+    height:45px;
+    width: 280px;
+    float:left;
+    border-radius: 10px;
+    margin: 160px 0 0 30px;
+    background-color:red
+}
+.nut_share{
+    height:45px;
+    width: 60px;
+    float:left;
+    border-radius: 10px;
+    margin: 160px 0 0 10px;
+    background-color:red
+}
+.nut_bacham{
+    height:45px;
+    width: 60px;
+    float:left;
+    border-radius: 10px;
+    margin: 160px 0 0 10px;
+    background-color:red
+}
+</style>
 <body>
 <div class="gop_2_menu">
-  
-da luu</div>
+    <div class="layout"> 
+        <div class="anh"></div> 
+        <div class="nut_them"> </div>
+        <div class="nut_share"> </div>
+        <div class="nut_bacham"> </div>
+    </div>
+    <div class="layout"> 
+        <div class="anh"></div> 
+        <div class="nut_them"> </div>
+        <div class="nut_share"> </div>
+        <div class="nut_bacham"> </div>
+    </div>
+    <div class="layout"> 
+        <div class="anh"></div> 
+        <div class="nut_them"> </div>
+        <div class="nut_share"> </div>
+        <div class="nut_bacham"> </div>
+    </div>
 </div>
 </body>
 
--- menu/khampha.php
+++ menu/khampha.php
@@ -1,1 +1,20 @@
-khampha
+<style>
+.khung{
+    height: 400px;
+    width: 400px;
+    float: left;
+    margin: 20px 10px -10px 0;
+    background-color:blue
+}
+
+</style>
+<body>
+    <div class="gop_2_menu">
+        <div class="khung"></div>
+        <div class="khung"></div>
+        <div class="khung"></div>
+        <div class="khung"></div>
+        <div class="khung"></div>
+        <div class="khung"></div>
+    </div>
+</body>
